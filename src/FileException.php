cd06fed30df50890142f3665d2f19c60c3f495c1
ZoDream <648383079@qq.com>
2017-08-09 23:29:14 +0800

    上传

---
+++ src/FileException.php
@@ -0,0 +1,18 @@
+<?php
+namespace Zodream\Disk;
+
+use Exception;
+
+class FileException extends Exception {
+
+    public function __construct($message, $code = 99, Exception $previous = null) {
+        if ($message instanceof FileObject) {
+            $message .= ' HAS ERROR!';
+        }
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getName() {
+        return 'FileException';
+    }
+}
