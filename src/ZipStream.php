603ed8a0595a61c81580b20c6e2b33025bf2fce5
zodream <648383079@qq.com>
2018-10-29 18:35:57 +0800

    修改

--- src/ZipStream.php
+++ src/ZipStream.php
@@ -17,8 +17,13 @@ class ZipStream {
         }
     }
 
-    public function open($file) {
-        $this->zip->open((string)$file);
+    public function open($file, $flags = null) {
+        $this->zip->open((string)$file, $flags);
+        return $this;
+    }
+
+    public function create($file) {
+        $this->zip->open((string)$file, ZipArchive::CREATE);
         return $this;
     }
 
@@ -27,12 +32,45 @@ class ZipStream {
         return $this;
     }
 
+    public function addDirectory($name, Directory $root) {
+        $name = trim($name, '/');
+        if (!empty($name)) {
+            $name .= '/';
+        }
+        $root->map(function ($file) use ($name) {
+           if ($file instanceof Directory) {
+               $this->addDirectory($name.$file->getName(), $file);
+               return;
+           }
+           $this->addFile($name.$file->getName(), $file);
+        });
+        return $this;
+    }
+
+    public function addString($name, $content) {
+        $this->zip->addFromString($name, $content);
+        return $this;
+    }
+
+    public function extractTo($root) {
+        $this->zip->extractTo((string)$root);
+        return $this;
+    }
 
     public function comment($content = null) {
         if (is_null($content)) {
             return $this->zip->getArchiveComment();
         }
         $this->zip->setArchiveComment($content);
+        return $this;
+    }
+
+    public function close() {
+        $this->zip->close();
+    }
+
+    public function __call($name, $arguments) {
+        $this->zip->{$name}(...$arguments);
         return $this;
     }
 }
